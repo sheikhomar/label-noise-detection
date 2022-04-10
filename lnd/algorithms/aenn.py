c3e51eeef6d3b2e07ff2343f89e35291ff2d7760
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2022-04-10 17:32:26 +0200

    Implement All k-NN detector.

---
+++ lnd/algorithms/aenn.py
@@ -0,0 +1,32 @@
+from typing import List
+import numpy as np
+import numba
+
+from sklearn.neighbors import KNeighborsClassifier
+
+from lnd.algorithms import Algorithm
+from lnd.data.dataset import DataSet
+from lnd.algorithms.enn import compute_knn
+
+
+class AllKEditedNearestNeighborDetector(Algorithm):
+    def __init__(self, max_k: int) -> None:
+        self._max_k = max_k
+    
+    def run(self, data_set: DataSet) -> List[int]:
+        X = data_set.features
+        y = data_set.labels
+        n_points = X.shape[0]
+        likely_mislabelled_indices = []
+        for k in range(1, self._max_k+1):
+            print(f"Running for k={k}")
+            for i in range(n_points):
+                pred_y = compute_knn(i, k, X, y)
+                given_y = y[i]
+                if pred_y != given_y:
+                    likely_mislabelled_indices.append(i)
+        return likely_mislabelled_indices
+
+
+def create() -> Algorithm:
+    return AllKEditedNearestNeighborDetector(max_k=11)
